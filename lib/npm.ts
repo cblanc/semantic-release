99bbd0d361f8ce5a80305696ff33d93e7874ab78
Christopher Blanchard <cablanchard@gmail.com>
2019-10-29 08:25:16 +0100

    feat(Versioning): Push npm version to github
    Once published to npm, push package.json with revised version number to github

--- lib/npm.ts
+++ lib/npm.ts
@@ -2,7 +2,18 @@ import * as release from "./index";
 
 const npmRelease = {
   ...release,
-  plugins: [...release.plugins, "@semantic-release/npm"],
+  plugins: [
+    ...release.plugins,
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
 };
 
 export = npmRelease;
